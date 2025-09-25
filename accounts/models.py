2710091b9a5abfdf2b0d5a1d947053adfec88f0b
Mohammad Ali Taqvazadeh <m.a.taqvazadeh@gmail.com>
2025-09-25 11:31:46 +0330

    Add accounts app models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserRoles(models.Model):
+    class Roles(models.IntegerChoices):
+        Secretary = 1
+        EducationManager = 2
+        EducationEmployee = 3
+        FinancialManager = 4
+        FinancialEmployee = 5
+        Teacher = 6
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.PositiveSmallIntegerField(choices=Roles.choices)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=('user', 'role'), name='uix_userroles_user_role')
+        ]
