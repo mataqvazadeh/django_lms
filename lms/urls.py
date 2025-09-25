d4972d6782ec3a4954d5ce72e68734e38c918a3c
Mohammad Ali Taqvazadeh <m.a.taqvazadeh@gmail.com>
2025-09-25 11:05:11 +0330

    Change LLM => LMS

--- lms/urls.py
+++ lms/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for llm project.
+URL configuration for lms project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
