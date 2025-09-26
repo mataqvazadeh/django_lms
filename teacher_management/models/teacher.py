2bcf2781fc36d0fb023f0030752621f64b8e6609
Mohammad Ali Taqvazadeh <m.a.taqvazadeh@gmail.com>
2025-09-26 10:51:30 +0330

    Fix model bug

--- teacher_management/models/teacher.py
+++ teacher_management/models/teacher.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 
 class Teacher(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     phone = models.TextField(max_length=20)
     emergency_phone = models.TextField(max_length=20)
