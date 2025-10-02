4b8b59e2ca8b790a637cc82c2d226142e7b5c183
Mohammad Ali Taqvazadeh <m.a.taqvazadeh@gmail.com>
2025-10-02 10:20:57 +0330

    Add userrole model to admin panel

--- accounts/models.py
+++ accounts/models.py
@@ -3,12 +3,12 @@ from django.conf import settings
 
 class UserRoles(models.Model):
     class Roles(models.IntegerChoices):
-        Secretary = 1
+        Secretary = 1, "منشی"
         EducationManager = 2
         EducationEmployee = 3
         FinancialManager = 4
         FinancialEmployee = 5
-        Teacher = 6
+        Teacher = 6, "مربی"
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     role = models.PositiveSmallIntegerField(choices=Roles.choices)
@@ -17,3 +17,6 @@ class UserRoles(models.Model):
         constraints = [
             models.UniqueConstraint(fields=('user', 'role'), name='uix_userroles_user_role')
         ]
+
+    def __str__(self):
+        return f'{self.user.get_full_name()}, {self.get_role_display()}'
